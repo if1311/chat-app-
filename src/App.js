15e72646e036d5e4dcb321507bdda42919345c6e
Ioana Alexandra Filip <ioannaf.@192-168-0-116.rdsnet.ro>
2020-03-30 16:59:42 +0300

    new quest added props and propType

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './App.css';
 import  Contact from './components/Contact';
@@ -6,9 +7,20 @@ import  Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact name='Minnie Porter' avatar='https://randomuser.me/api/portraits/women/31.jpg' online />
+      <Contact name='Harry Herrera' avatar='https://randomuser.me/api/portraits/men/55.jpg' offline />
+      <Contact name='Same Carr' avatar='https://randomuser.me/api/portraits/men/94.jpg' online />
     </div>
   );
 }
+
+
+
+Contact.propTypes = {
+  name: PropTypes.string,
+  avatar: PropTypes.string,
+  online: PropTypes.bool
+
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,19 +3,19 @@ import './Contact.css';
 
 
 
-const contactOnline = true;
 
 
-function Contact() { 
+
+function Contact(props) { 
     return (
         <header className= 'Contact'>
-            <img className= 'avatar' src= 'https://randomuser.me/api/portraits/women/31.jpg' alt= 'avartar of a woman' />
+            <img className= 'avatar' src= {props.avatar} alt= 'avartar of a woman' />
             <div>
-                <h4 className= 'name'> Minnie Porter </h4>
+                <h4 className= 'name'> {props.name} </h4>
                 <div className= 'status'>
-                    <span className= {contactOnline? 'status-online':'status-offline'}></span>
+                    <span className= {props.online? 'status-online':'status-offline'}></span>
                     {/* <p className= 'status-offline'></p> */}
-                    <p className= 'status-text'> {contactOnline? 'Online': 'Offline'} </p>
+                    <p className= 'status-text'> {props.online? 'Online': 'Offline'} </p>
                 </div>
             </div>
 
