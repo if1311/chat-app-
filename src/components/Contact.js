8a23be7fdd2eaf4b42765291a21e5fa54725e0eb
Ioana Alexandra Filip <ioannaf.@192-168-0-116.rdsnet.ro>
2020-04-01 07:39:23 +0300

    crated a class and a onclick event

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
+// function Contact(props) {
+//     return (
+//         <header className= 'Contact'>
+//             <img className= 'avatar' src= {props.avatar} alt= 'avartar of a woman' />
+//             <div>
+//                 <h4 className= 'name'> {props.name} </h4>
+//                 <div className= 'status'>
+//                     <span className= {props.online? 'status-online':'status-offline'}></span>
+//                     {/* <p className= 'status-offline'></p> */}
+//                     <p className= 'status-text'> {props.online? 'Online': 'Offline'} </p>
+//                 </div>
+//             </div>
 
+//         </header>
+//     );
+// }
 
-
-
-
-
-function Contact(props) { 
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+  render() {
     return (
-        <header className= 'Contact'>
-            <img className= 'avatar' src= {props.avatar} alt= 'avartar of a woman' />
-            <div>
-                <h4 className= 'name'> {props.name} </h4>
-                <div className= 'status'>
-                    <span className= {props.online? 'status-online':'status-offline'}></span>
-                    {/* <p className= 'status-offline'></p> */}
-                    <p className= 'status-text'> {props.online? 'Online': 'Offline'} </p>
-                </div>
-            </div>
-
-        </header>
+      <header className="Contact">
+        <img
+          className="avatar"
+          src={this.props.avatar}
+          alt="avartar of a woman"
+        />
+        <div>
+          <h4 className="name"> {this.props.name} </h4>
+          <div className="status">
+            <span
+              onClick={event => {
+                this.setState({ online: true });
+              }}
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></span>
+            {/* <p className= 'status-offline'></p> */}
+            <p className="status-text">
+              {" "}
+              {this.state.online ? "Online" : "Offline"}{" "}
+            </p>
+          </div>
+        </div>
+      </header>
     );
+  }
 }
 
-
 export default Contact;
-
-
-
-
